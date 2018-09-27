0ebe76d249af0b3314c8675d66af03931d4b9b8e
NatanielPont <npontcastells@gmail.com>
2018-09-27 19:59:38 +0200

    intorduction to work with laravel

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//controlador tasques
+//TDD-> test driven development
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about',function (){
+    return view('about');
+});
+
+//Route::view();
+Route::view('/contact', 'contact');
+
+
+//Route::get('/prova','ProvaController@show');
+Route::get('/prova',function (){
+        $prova='hola tt';
+        dd($prova);
+//    echo '<h1>Hola mon</h1>';
+
+
+});
+
+Route::redirect('/hola','prova');
