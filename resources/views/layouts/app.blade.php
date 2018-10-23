33b20301771fc67fd44c3ee20cc849cd036c8785
NatanielPont <npontcastells@gmail.com>
2018-10-23 21:28:21 +0200

    Sync

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -21,39 +23,66 @@
             dark
     >
         <v-list dense>
-            <v-list-tile href="/tasks">
-                <v-list-tile-action>
-                    <v-icon>assignment</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>Tasques PHP</v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile href="/tasks_vue">
-                <v-list-tile-action>
-                    <v-icon>assignment</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>Tasques Vue</v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile href="/about">
-                <v-list-tile-action>
-                    <v-icon>account_box</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>Sobre nosaltres</v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile href="/calendari">
-                <v-list-tile-action>
-                    <v-icon>calendar_today</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>Calendari</v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
+            <template v-for="item in items">
+                <v-layout
+                        v-if="item.heading"
+                        :key="item.heading"
+                        row
+                        align-center
+                >
+                    <v-flex xs6>
+                        <v-subheader v-if="item.heading">
+                            @{{ item.heading }}
+                        </v-subheader>
+                    </v-flex>
+                    <v-flex xs6 class="text-xs-center">
+                        <a href="#!" class="body-2 black--text">EDIT</a>
+                    </v-flex>
+                </v-layout>
+                <v-list-group
+                        v-else-if="item.children"
+                        v-model="item.model"
+                        :key="item.text"
+                        :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                        append-icon=""
+                >
+                    <v-list-tile   slot="activator" >
+                        <v-list-tile-content :href="item.url">
+                            <v-list-tile-title>
+                                @{{ item.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile
+                            v-for="(child, i) in item.children"
+                            :key="i"
+                            @click=""
+                    >
+                        <v-list-tile-action v-if="child.icon">
+                            <v-icon>@{{ child.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>
+                                @{{ child.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list-group>
+                <v-list-tile v-else :key="item.text" @click="">
+                    <v-list-tile-action>
+                        <v-icon>@{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            @{{ item.text }}
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </template>
         </v-list>
+
+
+
     </v-navigation-drawer>
     <v-toolbar color="indigo" dark fixed app>
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
@@ -72,7 +101,7 @@
         </v-container>
     </v-content>
     <v-footer color="indigo" app>
-        <span class="white--text">Created by Sergi Baucells Rodríguez, &copy; 2018 All rights reserved</span>
+        <span class="white--text">Created by Nataniel Pont Castells, &copy; 2018 All rights reserved</span>
     </v-footer>
 </v-app>
 
