6a7e2cc9d189b44a7e6f5b375a95e3bbef420ed7
NatanielPont <npontcastells@gmail.com>
2019-01-09 00:30:47 +0100

    Sync

---
+++ app/Http/Requests/UserTasksIndex.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 9/01/19
+ * Time: 0:17
+ */
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;
+
+class UserTasksIndex
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::user()->can('user.tasks.index');
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+}
