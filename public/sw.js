60abac29e10d133cfc74c561440feeee8239e541
NatanielPont <npontcastells@gmail.com>
2019-02-26 21:09:21 +0100

    Sync

---
+++ public/sw.js
@@ -0,0 +1,49 @@
+importScripts("/service-worker/precache-manifest.3107cdc05c6a5d480816c4daf95bdc8d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.skipWaiting()
+workbox.clientsClaim()
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://hacker-news.firebaseio.com'),
+//   workbox.strategies.staleWhileRevalidate()
+// );
+
+// TODO cal utilitzar PushManager al registrar el service worker
+self.addEventListener('push', (event) => {
+  const title = 'TODO CANVIAR TITOL'
+  const options = {
+    body: event.data.text()
+  }
+  event.waitUntil(self.registration.showNotification(title, options))
+})
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest)
+// workbox.precaching.precacheAndRoute([]) També funciona i workbox substitueix pel que pertoca -> placeholder
+
+// static
+// workbox.routing.registerRoute(
+//   new RegExp('.(?:ico)$'),
+//   workbox.strategies.networkFirst({
+//     cacheName: 'icons'
+//   })
+// )
+
+// images
+workbox.routing.registerRoute(
+  new RegExp('.(?:jpg|jpeg|png|gif|svg|webp)$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        purgeOnQuotaError: true
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  '/',
+  workbox.strategies.staleWhileRevalidate({ cacheName: 'landing' })
+)
+
--- resources/js/components/ServiceWorker.vue
+++ resources/js/components/ServiceWorker.vue
@@ -1,16 +1,27 @@
 <template>
   <span></span>
 </template>
+
 <script>
 export default {
   name: 'ServiceWorker',
-  // VULL EXECUTAR EL REGISTRE DEL SERVICE WORKER
-  mounted () {
-    if ('serviceWorker' in navigator) {
+  methods: {
+    registerServiceWorker () {
+      if (!('serviceWorker' in navigator)) {
+        console.log('Service workers aren\'t supported in this browser.')
+        return
+      }
       navigator.serviceWorker.register('/service-worker.js')
-    } else {
-      console.log('Navegador obsolet')
+        .then(function (registration) {
+          console.log('Registration successful, scope is:', registration.scope)
+        })
+        .catch(function (error) {
+          console.log('Service worker registration failed, error:', error)
+        })
     }
+  },
+  mounted () {
+    this.registerServiceWorker()
   }
 }
 </script>
