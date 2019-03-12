35f3a59711c03b0afad129c3bd0c9d8db9a8e179
NatanielPont <npontcastells@gmail.com>
2019-03-12 21:21:29 +0100

    Sync

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -11,6 +11,13 @@
     <link rel="manifest" href="/manifest.json">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png">
+    <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#5bbad5">
+    <meta name="theme-color" content="#5bbad5">
+
     <script defer src="{{ mix('/js/manifest.js') }}"></script>
     <script defer src="{{ mix('/js/vendor.js') }}"></script>
     <script defer src="{{ mix('/js/app.js') }}"></script>
