bbab9f59f6d506c4af4a5880efd14fea267e432b
NatanielPont <npontcastells@gmail.com>
2019-03-20 02:33:07 +0100

    pep

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.550c18b1862d4104bbbd411e9403e6b3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.1e7bee4fa9c4973be9c7155d42cdc5ba.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting()
 workbox.clientsClaim()
