27a6a83d7282ed2ff89f0aba5123118c06a53d98
NatanielPont <npontcastells@gmail.com>
2019-05-26 05:45:41 +0200

    Sync

--- routes/channels.php
+++ routes/channels.php
@@ -28,13 +28,13 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
 Broadcast::channel('Tasques', function ($user) {
     return $user->isSuperAdmin() || $user->hasRole('TaskManager');
 });
-Broadcast::channel('App.Counter', function ($user) {
-    return [
-        'id' => $user->id,
-        'name' => $user->name,
-        'gravatar' => $user->gravatar
-    ];
-});
+//Broadcast::channel('App.Counter', function ($user) {
+//    return [
+//        'id' => $user->id,
+//        'name' => $user->name,
+//        'gravatar' => $user->gravatar
+//    ];
+//});
 Broadcast::channel('App.Log', function () {
     return true;
 });
