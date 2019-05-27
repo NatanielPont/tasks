b75c0f1796da553ca7d62374b1ebe9a1c58d02c3
NatanielPont <npontcastells@gmail.com>
2019-05-27 17:51:16 +0200

    Sync

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.f9928d9643e013650c33dce3097353ea.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.4e7290e41ed2b241a7c50d2d1855bd9f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.setConfig({
   debug: true
